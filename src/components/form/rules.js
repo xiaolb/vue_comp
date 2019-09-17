8cb9847f183ab9f25620c7ff1fc7cc5c8035e484
xiaolibin <xiaolibin4186@tops001.com>
2019-09-17 16:50:56 +0800

    intNumber只支持整数

--- src/components/form/rules.js
+++ src/components/form/rules.js
@@ -13,7 +13,7 @@ export function checkIdCard(rule, value, callback) {
 export const formRules = {
     idCard: { validator: checkIdCard, trigger: ['blur', 'change'] },
     phone: { pattern: /^1\d{10}$/, message: '目前只支持中国大陆的手机号码', trigger: ['blur', 'change'] },
-    intNumber: { pattern: /^-?[1-9]\d*$/, message: '只支持整数', trigger: ['blur', 'change'] },
+    intNumber: { pattern: /^(\-|\+)?\d+$/, message: '只支持整数', trigger: ['blur', 'change'] },
     floatNumber: { pattern: /^-?([1-9]\d*\.\d*|0\.\d*[1-9]\d*|0?\.0+|0)$/, message: '只支持浮点数', trigger: ['blur', 'change'] },
     number: { pattern: /^(\-|\+)?\d+(\.\d+)?$/, message: '只支持数字', trigger: ['blur', 'change'] },
     email: { type: 'email', message: '请输入正确的邮箱地址', trigger: ['blur', 'change'] },
